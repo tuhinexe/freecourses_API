13362d9cc6c9d781499161b49a4fd61d4cf0d787
TuhinBar <tuhinbr99@gmail.com>
2022-08-31 21:05:12 +0530

    Free Courses API

---
+++ app.js
@@ -0,0 +1,75 @@
+// Requiring the modules
+
+const express = require("express");
+const app = express();
+const courseApi = require(__dirname + "/endpoint");
+
+app.get("/", async (req, res) => {
+	  const data = await courseApi.fetchDailyCources();
+  res.send(data);
+});
+
+app.get("/courses", async (req, res) => {
+	  const data = await courseApi.fetchDailyCources();
+	    res.json({courses : {course1: data.results[0],course2: data.results[1],course3: data.results[2], course4: data.results[3], course5: data.results[4], course6: data.results[5], course7: data.results[6], course8: data.results[7], course9: data.results[8], course10: data.results[9]}});
+});
+
+app.get("/courses/course1", async (req, res) => {
+	  const data = await courseApi.fetchDailyCources();
+	    res.json({course1: data.results[0]});
+});
+
+app.get("/courses/course2", async (req, res) => {
+	  const data = await courseApi.fetchDailyCources();
+	    res.json({course2: data.results[1]});
+});
+
+app.get("/courses/course3", async (req, res) => {
+	  const data = await courseApi.fetchDailyCources();
+	    res.json({course3: data.results[2]});
+});
+
+app.get("/courses/course4", async (req, res) => {
+	  const data = await courseApi.fetchDailyCources();
+	    res.json({course4: data.results[3]});
+});
+
+app.get("/courses/course5", async (req, res) => {
+	  const data = await courseApi.fetchDailyCources();
+	    res.json({course5: data.results[4]});
+});
+
+app.get("/courses/course6", async (req, res) => {
+	  const data = await courseApi.fetchDailyCources();
+	    res.json({course6: data.results[5]});
+});
+
+app.get("/courses/course7", async (req, res) => {
+	  const data = await courseApi.fetchDailyCources();
+	    res.json({course7: data.results[6]});
+});
+
+app.get("/courses/course8", async (req, res) => {
+	  const data = await courseApi.fetchDailyCources();
+	    res.json({course8: data.results[7]});
+});
+
+app.get("/courses/course9", async (req, res) => {
+	  const data = await courseApi.fetchDailyCources();
+	    res.json({course9: data.results[8]});
+});
+
+app.get("/courses/course10", async (req, res) => {
+	  const data = await courseApi.fetchDailyCources();
+	    res.json({course10: data.results[9]});
+});
+
+// Definig the port to listen on
+let port = process.env.PORT;
+if (port == null || port == ""){
+  port = 3000;
+}
+
+app.listen(port, () => {
+  console.log("listening on port " + port);
+});
---
+++ endpoint.js
@@ -0,0 +1,17 @@
+const fetch = require("node-fetch");
+const UDEMY_API_ENDPOINT =
+  "https://www.real.discount/api-web/all-courses/?store=&page=1&per_page=10&orderby=undefined&free=1&search=&language=&cat=&subcat=&editorschoices=";
+/*sync  */
+module.exports = {fetchDailyCources : async () => {
+  console.log(`Fetching daily coding challenge from GFG api.`);
+
+  const init = {
+    method: "GET",
+    headers: { "Content-Type": "application/json" }
+  };
+
+  const response = await fetch(UDEMY_API_ENDPOINT, init);
+
+  return response.json();
+},
+};
