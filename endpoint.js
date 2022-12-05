d819ec7ac1618591597e236ab8b016d957a3c971
Tuhin <85868593+TuhinBar@users.noreply.github.com>
2022-12-05 19:09:55 +0530

    Update endpoint.js

--- endpoint.js
+++ endpoint.js
@@ -1,7 +1,7 @@
 const fetch = require("node-fetch");
 const UDEMY_API_ENDPOINT =
   "https://www.real.discount/api-web/all-courses/?store=&page=1&per_page=10&orderby=undefined&free=1&search=&language=&cat=&subcat=&editorschoices=";
-/*sync  */
+
 module.exports = {fetchDailyCources : async () => {
   console.log(`Fetching daily coding challenge from GFG api.`);
 
