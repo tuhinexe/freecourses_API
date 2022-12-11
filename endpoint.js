5d8771609a51621a2272bfe3d777a7fdf0809d47
TuhinBar <tuhinbr99@gmail.com>
2022-12-11 23:23:07 +0530

    Added vercel config

--- endpoint.js
+++ endpoint.js
@@ -3,7 +3,6 @@ const UDEMY_API_ENDPOINT =
   "https://www.real.discount/api-web/all-courses/?store=&page=1&per_page=10&orderby=undefined&free=1&search=&language=&cat=&subcat=&editorschoices=";
 
 module.exports = {fetchDailyCources : async () => {
-  console.log(`Fetching daily coding challenge from GFG api.`);
 
   const init = {
     method: "GET",
